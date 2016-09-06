e4d58dcbe63bfc37542a58bef4e03dbd36074e41
Gary O'Donoghue <Gary@Garys-MacBook-Air.local>
2016-09-06 22:01:35 +0100

    Initial Commit

---
+++ GalwayBus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GalwayBus
+//
+//  Created by Gary O'Donoghue on 06/09/2016.
+//  Copyright (c) 2016 Gary O'Donoghue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ GalwayBusTests/GalwayBusTests.swift
@@ -0,0 +1,36 @@
+//
+//  GalwayBusTests.swift
+//  GalwayBusTests
+//
+//  Created by Gary O'Donoghue on 06/09/2016.
+//  Copyright (c) 2016 Gary O'Donoghue. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class GalwayBusTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
